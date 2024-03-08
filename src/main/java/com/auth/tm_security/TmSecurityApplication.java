2f90d38305bbbf90e3d4dcc9ba0a28e701063b81
Sonechka <killerastren@gmail.com>
2024-03-09 00:09:03 +0700

    1# Сервис авторизации на jwt токенах (сырой)

---
+++ src/main/java/com/auth/tm_security/TmSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.auth.tm_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auth/tm_security/TmSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.tm_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
